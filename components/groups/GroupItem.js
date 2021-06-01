ba112b5858758e25d055377c69ee7b96a8237679
mhans003 <michaeledwardhanson@gmail.com>
2021-06-01 08:00:06 -0700

    Clicking on details button now renders placeholder data to show dynamic routes working; Styles added for group detail page.

--- components/groups/GroupItem.js
+++ components/groups/GroupItem.js
@@ -1,7 +1,20 @@
+import { useRouter } from 'next/router';
+
 import Card from '../ui/Card';
 import classes from './GroupItem.module.css';
 
 const GroupItem = props => {
+    //Use next router object to create links to page details.
+    const router = useRouter();
+
+    console.log(props.id);
+
+    //Show the details for a particular group.
+    const handleShowDetails = () => {
+        //Use the router object to dynamically create and navigate to a new URL for this group.
+        router.push(`/${props.id}`);
+    }
+
     return (
         <li className={classes.item}>
             <Card>
@@ -13,7 +26,7 @@ const GroupItem = props => {
                     <address>{props.address}</address>
                 </div>
                 <div className={classes.actions}>
-                    <button>Show Details</button>
+                    <button onClick={handleShowDetails}>Show Details</button>
                 </div>
             </Card>
         </li>
---
+++ pages/[groupId].js
@@ -0,0 +1,14 @@
+import GroupDetail from '../components/groups/GroupDetail';
+
+const Group = () => {
+    return (
+        <GroupDetail
+            image="https://images.unsplash.com/photo-1593642634402-b0eb5e2eebc9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80"
+            title="Group 1"
+            address="placeholder address"
+            description="placeholder description"
+        />
+    );
+};
+
+export default Group;
