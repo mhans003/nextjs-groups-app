1408f33294b94b99d1e28452a5f07f8b34a72fe4
mhans003 <michaeledwardhanson@gmail.com>
2021-06-03 10:36:26 -0700

    Wrapped POST handler logic into try/catch block

--- pages/api/new-group.js
+++ pages/api/new-group.js
@@ -6,19 +6,24 @@ import { MongoClient } from 'mongodb';
 const handler = async (req, res) => {
     //Create new group.
     if (req.method === 'POST') {
-        //Pull out request body.
-        const data = req.body;
-        //Connect to DB and create client object.
-        console.log(process.env.DB_CONNECTION)
-        const client = await MongoClient.connect(process.env.DB_CONNECTION);
-        const db = client.db();
-        //Identify collection.
-        const groupsCollection = db.collection('groups');
-        //Insert into this collection.
-        const result = await groupsCollection.insertOne(data);
-        console.log(result);
-        //Send back response.
-        res.status(201).json({message: "Group Inserted"});
+        try {
+            //Pull out request body.
+            const data = req.body;
+            //Connect to DB and create client object.
+            console.log(process.env.DB_CONNECTION)
+            const client = await MongoClient.connect(process.env.DB_CONNECTION);
+            const db = client.db();
+            //Identify collection.
+            const groupsCollection = db.collection('groups');
+            //Insert into this collection.
+            const result = await groupsCollection.insertOne(data);
+            console.log(result);
+            //Send back response.
+            res.status(201).json({message: "Group Inserted"});
+        } catch(error) {
+            console.log(error);
+            res.status(500).json({message: "An error occurred while inserting records."});
+        }
     }
 }
 
