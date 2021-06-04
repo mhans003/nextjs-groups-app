b015b9c4f009ae1b99d49e8ea035f8954153a6f9
mhans003 <michaeledwardhanson@gmail.com>
2021-06-04 05:33:08 -0700

    Added code to connect/retrieve from MongoDB in index page to get pre-rendered homepage with group data, and got rid of placeholder data there.

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,5 @@
+import { MongoClient } from 'mongodb';
 import GroupList from '../components/groups/GroupList';
-//Temporary placeholder data to pass into the GroupsList component to render.
-const placeholderData = [
-    {
-        id: "g1",
-        title: "Awesome Group 1",
-        image: "https://images.unsplash.com/photo-1593642634402-b0eb5e2eebc9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80",
-        address: "555 Street City, State",
-        description: "This is a great group!"
-    },
-    {
-        id: "g2",
-        title: "Super Group 2",
-        image: "https://images.unsplash.com/photo-1619537906775-d79054299429?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80",
-        address: "555 Street City, State",
-        description: "This is a super group!"
-    }
-]
 
 //Props come from pre-rendering process on the server.
 const Home = props => {
@@ -27,12 +11,26 @@ const Home = props => {
 //Instead, data is retrieved on server and sent to this component via props.
 //Alternative is to use getServerSideProps, which doesn't run on build, but always on server after deployment.
 export async function getStaticProps() {
-    //Fetch data.
+    //Fetch data to get groups.
+    //Since this code will run on the server, we can connect to MongoDB here directly.
+    const client = await MongoClient.connect(process.env.DB_CONNECTION);
+    const db = client.db();
+    //Identify collection.
+    const groupsCollection = db.collection('groups');
+    //get the stored data as an array.
+    const groups = await groupsCollection.find().toArray();
+    //close the connection.
+    client.close();
 
-    //For now, just use the placeholder data.
     return {
+        //Map through the fetched group data to transform the ID field and grab all properties.
         props: {
-            groups: placeholderData
+            groups: groups.map(group => ({
+                title: group.title,
+                address: group.address,
+                image: group.image,
+                id: group._id.toString()
+            }))
         },
         //Revalidate property utilizes incremental static generation.
         //Takes the number of seconds to wait before generating another request.
