18edc22f94e75692c87adc2647f0d9bfdc9b4230
mhans003 <michaeledwardhanson@gmail.com>
2021-06-28 08:36:34 -0700

    React Bootstrap Navbar now has color scheme, including link hover; Add New Group replaced with fontawesome icon; Fontawesome CDN included in project.

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -10,17 +10,17 @@ import NavLink from 'react-bootstrap/NavLink';
 const MainNavigation = () => {
 
     return (
-        <Navbar bg="light" expand="lg" className={classes.navContainer}>
+        <Navbar expand="md" className={classes.navContainer}>
             <Container>
-                <Navbar.Brand className={classes.logo}><Link href='/'>NextGroup</Link></Navbar.Brand>
+                <Navbar.Brand className={[classes.logo, classes.navigationLink]}><Link href='/'>NextGroup</Link></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="justify-content-end" style={{ width: "100%"}}>
                         <NavItem>
-                            <NavLink><Link href='/'>All Groups</Link></NavLink>
+                            <NavLink className={[classes.navigationItem, classes.navigationLink]}><Link href='/'>All Groups</Link></NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink><Link href='/new-group'>New Group</Link></NavLink>
+                            <NavLink className={[classes.navigationItem, classes.navigationLink]} title="Add New Group"><Link href='/new-group'><i className="fal fa-plus-circle" style={{ fontSize: "2rem" }}></i></Link></NavLink>
                         </NavItem>
                     </Nav>
                 </Navbar.Collapse>
