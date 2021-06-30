8d5d800742d4c17344cbfd678135026509b2dc5f
mhans003 <michaeledwardhanson@gmail.com>
2021-06-30 06:40:25 -0700

    Added react-bootstrap grid system to Group List

--- components/groups/GroupList.js
+++ components/groups/GroupList.js
@@ -1,18 +1,25 @@
 import GroupItem from './GroupItem';
 import classes from './GroupList.module.css';
+import {Container, Row, Col} from 'react-bootstrap';
 
 const GroupList = props => {
     return (
         <ul className={classes.list}>
-            {props.groups.map(group => (
-                <GroupItem
-                    key={group.id}
-                    id={group.id}
-                    image={group.image}
-                    title={group.title}
-                    address={group.address}
-                />
-            ))}
+            <Container>
+                <Row>
+                    {props.groups.map(group => (
+                        <Col md="6">
+                            <GroupItem
+                                key={group.id}
+                                id={group.id}
+                                image={group.image}
+                                title={group.title}
+                                address={group.address}
+                            />
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
         </ul>
     );
 }
